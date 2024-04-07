35fdb58356c6e8447471e9f13da01466d0eef0bc
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-04-07 17:52:43 +0530

    ghg

---
+++ recipeapp/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    input type
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+const meals = computed(() => StorageEvent.state.meals)
+
+</script>
